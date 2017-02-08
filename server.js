a0c52e97c1d3bb074aa43a5cf24bfdcd1c1e1e32
rohankumar24 <prasadrohan04@gmail.com>
2017-02-08 14:31:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/article-one', function(req,res){
  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/calculator', function(req,res){
+app.get('/Calculator', function(req,res){
  res.sendFile(path.join(__dirname, 'ui', 'Calculator.html'));
 });
 app.get('/article-two', function(req,res){
